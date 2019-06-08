9087c66867e6d13c49c0ad0f2cc6c440c4369e3f
Peter Hudel <phudel@uoguelph.ca>
2019-06-08 15:29:00 -0400

    Added config file

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+  	default: "arcade",
+  	arcade: {
+  		gravity: {y: 0},
+  		debug: true
+  	}
+  },
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
