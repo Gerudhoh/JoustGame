c1479696638c07d99a8b0b45435234b89f6f7c6a
Julia Hohenadel <julia.hohenadel@magnetforensics.com>
2019-06-13 22:51:28 -0400

    Trying to add movement

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -17,34 +17,52 @@ class GameScreen extends Phaser.Scene {
 	}
 
 	create() {
-	 	
+		 
+		jousterRight = this.add.sprite(700, 400, "sword1", 5);
+		jousterLeft = this.add.sprite(100, 450, "sword2", 0);
 		  let titleText = this.add.text(50, 550, "Now Playing Game");
 		  this.add.image(400, 300, 'background');
-		  
-		  let jousterRight = this.add.sprite(700, 400, "sword1", 5);
+
 		  this.anims.create({
-			key: 'running1',
+			key: 'runningLeft',
 			//repeat -1 means it loops
    
 				 repeat: -1,
 				 frameRate: 10,
 				 frames: this.anims.generateFrameNames('sword1', {start: 0, end: 4})
 			 });
-			 jousterRight.play('running');
-
-			let jousterLeft = this.add.sprite(100, 450, "sword2", 0);
+			 jousterRight.play('runningLeft');
+			
 			 this.anims.create({
-				key: 'running2',
+				key: 'runningRight',
 				//repeat -1 means it loops
 	   
 					 repeat: -1,
 					 frameRate: 10,
 					 frames: this.anims.generateFrameNames('sword2', {start: 0, end: 4})
 				 });
-			  jousterLeft.play('running2');
+			  jousterLeft.play('runningRight');
 			
 	}
 
+/*	update() {
+	
+		jousterLeft.body.velocity.x = 0;
+		jousterRight.body.velocity.x = 0;
+	
+		if (cursors.left.isDown)
+		{
+			jousterRight.body.velocity.x = -150;
+			jousterRight.play('runningLeft');
+		}
+		else
+		{
+			jousterRight.body.velocity.x = 0;
+			jousterRight.animations.stop();
+
+		}
+	
+	}*/
 }
 
 //exports the scene so config can load it
